8bd898ba6dddd038810af85386b53f4a5ff6af1f
C. Dylan Shearer <dylan@nekonya.info>
2014-09-13 17:09:39 -0700

    Added basics of arg parsing to client.

--- client/client.go
+++ client/client.go
@@ -6,47 +6,86 @@ import (
     "net/rpc"
     "fmt"
     "os"
+    "flag"
 )
 
+const (
+    ListCmdStr = "list"
+    LogCmdStr  = "log"
+    StopCmdStr = "stop"
+)
+
+func usage() {
+    fmt.Printf("\nUsage: %v [flags] (%v|%v|%v)\nFlags:\n", os.Args[0], ListCmdStr, LogCmdStr, StopCmdStr)
+    flag.PrintDefaults()
+}
+
 func main() {
-    // connect to daemon
-    addr, err := net.ResolveUnixAddr("unix", jobber.DaemonSocketAddr)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Couldn't resolve Unix addr: %v\n", err)
-        os.Exit(1)
-    }
-    conn, err := net.DialUnix("unix", nil, addr)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Couldn't connect to daemon: %v\n", err)
-        os.Exit(1)
-    }
-    defer conn.Close()
-    rpcClient := rpc.NewClient(conn)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Couldn't make RPC client: %v\n", err)
-        os.Exit(1)
-    }
+    flag.Usage = usage
     
-    // call
-    var result string
+    var helpFlag_p = flag.Bool("h", false, "help")
+    flag.Parse()
     
-    err = rpcClient.Call("RealIpcServer.ListJobs", 1, &result)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "RPC failed: %v\n", err)
-        os.Exit(1)
+    if *helpFlag_p {
+        usage()
+        os.Exit(0)
+    } else {
+        if len(flag.Args()) == 0 {
+            fmt.Fprintf(os.Stderr, "Specify a command.\n")
+            flag.Usage()
+            os.Exit(1)
+        }
+        
+        // connect to daemon
+        addr, err := net.ResolveUnixAddr("unix", jobber.DaemonSocketAddr)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Couldn't resolve Unix addr: %v\n", err)
+            os.Exit(1)
+        }
+        conn, err := net.DialUnix("unix", nil, addr)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Couldn't connect to daemon: %v\n", err)
+            os.Exit(1)
+        }
+        defer conn.Close()
+        rpcClient := rpc.NewClient(conn)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Couldn't make RPC client: %v\n", err)
+            os.Exit(1)
+        }
+        
+        // do command
+        switch flag.Arg(0) {
+        case ListCmdStr:
+            var result string
+            err = rpcClient.Call("RealIpcServer.ListJobs", 1, &result)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "RPC failed: %v\n", err)
+                os.Exit(1)
+            }
+            fmt.Printf("%s\n", result)
+        
+        case LogCmdStr:
+            var result string
+            err = rpcClient.Call("RealIpcServer.ListHistory", 1, &result)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "RPC failed: %v\n", err)
+                os.Exit(1)
+            }
+            fmt.Printf("%s\n", result)
+        
+        case StopCmdStr:
+            var result string
+            err = rpcClient.Call("RealIpcServer.Stop", 1, &result)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "RPC failed: %v\n", err)
+                os.Exit(1)
+            }
+        
+        default:
+            fmt.Fprintf(os.Stderr, "Invalid command: \"%v\".\n", flag.Arg(0))
+            flag.Usage()
+            os.Exit(1)
+        }
     }
-    fmt.Printf("%s\n", result)
-    
-    err = rpcClient.Call("RealIpcServer.ListHistory", 1, &result)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "RPC failed: %v\n", err)
-        os.Exit(1)
-    }
-    fmt.Printf("%s\n", result)
-    
-/*    err = rpcClient.Call("RealIpcServer.Stop", 1, &result)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "RPC failed: %v\n", err)
-        os.Exit(1)
-    }*/
 }
