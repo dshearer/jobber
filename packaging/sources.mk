7190da2a6f84eb7af02456f5ce5ce65aa1636855
C. Dylan Shearer <dylan@nekonya.info>
2017-08-27 14:23:16 -0700

    Can build & test CentOS 6 RPM in VM

--- packaging/sources.mk
+++ packaging/sources.mk
@@ -1,12 +1,12 @@
 PACKAGING_SOURCES := \
 	Makefile \
-	centos_6.7/se_policy/jobber.fc \
-	centos_6.7/se_policy/jobber.if \
-	centos_6.7/se_policy/jobber.te \
-	centos_6.7/jobber_init \
-	centos_6.7/jobber.spec \
-	centos_6.7/Makefile \
-	centos_6.7/pkgrel \
+	centos_6/se_policy/jobber.fc \
+	centos_6/se_policy/jobber.if \
+	centos_6/se_policy/jobber.te \
+	centos_6/jobber_init \
+	centos_6/jobber.spec \
+	centos_6/Makefile \
+	centos_6/pkgrel \
 	alpine_3.3/APKBUILD \
 	alpine_3.3/jobber.initd \
 	alpine_3.3/jobber.post-deinstall \
