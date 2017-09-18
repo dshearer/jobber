078f56aaf9cbea2bc7eac105176f10fee052692d
C. Dylan Shearer <dylan@nekonya.info>
2017-09-17 19:01:04 -0700

    Enhance platform tests; enable parallel tests

--- packaging/head.mk
+++ packaging/head.mk
@@ -3,8 +3,6 @@ WORK_DIR ?= $(abspath work)
 SRC_ROOT := $(abspath ../..)
 VERSION := $(shell cat ${SRC_ROOT}/version)
 SRC_TARFILE = jobber-${VERSION}.tgz
-ROBOT_TESTS_DIR = ${SRC_ROOT}/platform_tests/robot_tests
-ROBOT_TESTS = $(wildcard ${ROBOT_TESTS_DIR}/*)
 
 # Must be defined by includers:
 #	Variables:
