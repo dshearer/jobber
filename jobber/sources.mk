2f194fc5ced05e61310eb044f3f7b4fe4af506ac
C. Dylan Shearer <dylan@nekonya.info>
2017-12-23 14:48:33 -0800

    jobber: Move cmds into own files

--- jobber/sources.mk
+++ jobber/sources.mk
@@ -1,8 +1,12 @@
 CLIENT_SOURCES := \
+	cat_cmd.go \
 	list_cmd.go \
 	log_cmd.go \
 	main.go \
-	reload_cmd.go
+	pause_cmd.go \
+	reload_cmd.go \
+	resume_cmd.go \
+	test_cmd.go
 
 CLIENT_TEST_SOURCES :=
 
