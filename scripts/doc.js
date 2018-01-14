c374915839002a9b1174b1bac643502263606b2e
C. Dylan Shearer <dylan@nekonya.info>
2018-01-13 16:35:06 -0800

    Support subsections in doc page nav bar

--- scripts/doc.js
+++ scripts/doc.js
@@ -38,19 +38,65 @@ function makeVersionsSelect(currVersion) {
 	return select;
 }
 
-function addSections(navUl, sectionContainer, sections) {
+function _makeDocNav(navUl, sections) {
 	for ( var sectId in sections) {
-		// make nav bar items
 		var section = sections[sectId];
+		
+		// make nav bar item
 		var li = $("<li class=\"nobr\"></li>").appendTo(navUl);
 		var a = $("<a target=\"_self\"></a>").appendTo(li);
 		a.attr("href", "#" + sectId);
 		a.text(section.title);
-
-		// load section
-		var div = $("<div></div>").appendTo(sectionContainer);
-		div.attr("id", sectId);
-		div.load("/jobber/doc/" + section.version + "/partials/" + 
-				section.page);
+		
+		if (section.sections) {
+			// add items for subsections
+			var subUl = $("<ul class=\"nav-list-3\"></ul>").appendTo(li);
+			for (var subSectId in section.sections)
+			{
+				var subSection = section.sections[subSectId];
+				
+				// make nav bar item
+				var subLi = $("<li class=\"nobr\"></li>").appendTo(subUl);
+				var subA = $("<a target=\"_self\"></a>").appendTo(subLi);
+				subA.attr("href", "#" + subSectId);
+				subA.text(subSection.title);
+			}
+		}
 	}
+}
+
+function _makeSections(sectionContainer, sections) {
+	for ( var sectId in sections) {
+		var section = sections[sectId];
+
+		if (section.sections) {
+			// make section
+			var div = $("<section></section>").appendTo(sectionContainer);
+			div.attr("id", sectId);
+			var header = $("<h2></h2>").appendTo(div);
+			header.text(section.title);
+			
+			// load subsections
+			for (var subSectId in section.sections)
+			{
+				var subSection = section.sections[subSectId];
+				var subDiv = $("<div></div>").appendTo(div);
+				subDiv.attr("id", subSectId);
+				subDiv.load("/jobber/doc/" + subSection.version + 
+						"/partials/" + subSection.page);
+			}
+		}
+		else {
+			// load section
+			var div = $("<div></div>").appendTo(sectionContainer);
+			div.attr("id", sectId);
+			div.load("/jobber/doc/" + section.version + "/partials/" + 
+					section.page);
+		}
+	}
+}
+
+function addSections(navUl, sectionContainer, sections) {
+	_makeDocNav(navUl, sections);
+	_makeSections(sectionContainer, sections);
 }
