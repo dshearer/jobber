d46dc53740d27f6331c889870d7a137af09eb740
C. Dylan Shearer <dylan@nekonya.info>
2018-01-27 13:24:52 -0800

    (#156) Make Ubuntu 14 package

--- packaging/sources.mk
+++ packaging/sources.mk
@@ -1,12 +1,14 @@
 include packaging/centos_6/sources.mk
 include packaging/centos_7/sources.mk
 include packaging/debian_9/sources.mk
+include packaging/ubuntu_14/sources.mk
 include packaging/ubuntu_16/sources.mk
 
 PACKAGING_SOURCES := \
 	${CENTOS_6_SOURCES} \
 	${CENTOS_7_SOURCES} \
 	${DEBIAN_9_SOURCES} \
+	${UBUNTU_14_SOURCES} \
 	${UBUNTU_16_SOURCES} \
 	packaging/head.mk \
 	packaging/Makefile \
