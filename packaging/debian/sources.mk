3cdbfb9f1e9565fb0d6489600320d29752e3331e
C. Dylan Shearer <dylan@nekonya.info>
2020-03-21 17:10:36 -0700

    GH actions: Build packages for Alpine and RHEL

---
+++ packaging/debian/sources.mk
@@ -0,0 +1,5 @@
+DEBIAN_SOURCES = \
+	packaging/debian/debian-pkg \
+	packaging/debian/Makefile \
+	packaging/debian/sources.mk \
+	packaging/debian/Vagrantfile
--- packaging/debian_9/sources.mk
+++
@@ -1,5 +0,0 @@
-DEBIAN_9_SOURCES = \
-	packaging/debian_9/debian-pkg \
-	packaging/debian_9/Makefile \
-	packaging/debian_9/sources.mk \
-	packaging/debian_9/Vagrantfile
