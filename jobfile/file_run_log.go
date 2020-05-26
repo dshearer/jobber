a0e22e67ba6e87fa5962b4e8ddda53c93501693c
C. Dylan Shearer <dylan@nekonya.info>
2020-05-25 17:05:21 -0700

    TestFileRunLog: Use temp dir for run log file
    Before, the file wasn't always getting deleted, and subsequent
    runs of the test would then load the entries in SetupTest.

--- jobfile/file_run_log.go
+++ jobfile/file_run_log.go
@@ -142,7 +142,7 @@ func (self *backingFileDtor) readEntry(idx int,
 	}
 	entry, err := decodeRunLogEntry(string(buf))
 	if err != nil {
-		msg := fmt.Sprintf("Entry %v in %v is bad", idx, self.path)
+		msg := fmt.Sprintf("Entry %v in %v is bad: %v", idx, self.path, err)
 		return nil, &common.Error{What: msg, Cause: err}
 	}
 	return entry, nil
@@ -928,8 +928,10 @@ func decodeRunLogEntry(s string) (*RunLogEntry, error) {
 
 	// split string into fields
 	fields := strings.Split(s, "\t")
-	if len(fields) != 5 {
-		msg := "Not enough fields in log entry line."
+	nbrFields := 5
+	if len(fields) != nbrFields {
+		msg := fmt.Sprintf("Wrong number of fields in log entry line: %v but expected %v",
+			len(fields), nbrFields)
 		return nil, &common.Error{What: msg}
 	}
 
